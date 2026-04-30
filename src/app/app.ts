deabc1d20bf181603257cc2250f453d3466afbf8
Victor Hugo <email.vbarr@gmail.com>
2026-04-29 19:59:43 -0600

    feat: Estructura de interfaz de conversion y ajustes de configuracion

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // <--- Importante para leer los inputs
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule], // <--- Agregamos FormsModule aquí
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('coinshift-app');
+export class AppComponent {
+  /* 
+     Autor: Victor Hugo Barraza Gonzalez
+     Proyecto: CoinShift - Lógica Inicial
+  */
+  
+  // Variables que se conectarán al HTML
+  amount: number = 0;
+  fromCurrency: string = 'USD';
+  toCurrency: string = 'MXN';
+  result: number = 0;
+
+  // Función de convercion
+  convertir() {
+    // FirstTest
+    if (this.fromCurrency === 'USD' && this.toCurrency === 'MXN') {
+      this.result = this.amount * 18.50; // Tasa estática temporal
+    } else if (this.fromCurrency === 'MXN' && this.toCurrency === 'USD') {
+      this.result = this.amount / 18.50;
+    } else {
+      this.result = this.amount; // Misma divisa
+    }
+  }
 }
